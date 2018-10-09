da82df743bacbf8e9297c2bbeb875fed50654639
klm123 <klm123@163.com>
2018-10-08 20:28:12 -0700

    reset settings and manage

--- manage.py
+++ manage.py
@@ -5,10 +5,16 @@ from django.core.management import execute_from_command_line
 from django.conf import settings as django_settings
 
 
-def load_django_settings():
+def load_django_settings(mode='develop'):
     settings = {}
     kwargs = {}
     mods = [importlib.import_module('admin.settings')]
+
+    if mode == "develop":
+        try:
+            mods.append(importlib.import_module('dev_settings'))
+        except ImportError:
+            pass
 
     if hasattr(mods[0], 'load_settings'):
         getattr(mods[0], 'load_settings')(settings, **kwargs)
