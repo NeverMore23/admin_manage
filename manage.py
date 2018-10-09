8b625be4fba8090ab6e365e2bbe512c37905ebfb
klm123 <klm123@163.com>
2018-10-08 20:21:09 -0700

    reset settings and manage

--- manage.py
+++ manage.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python
-import os
-import sys
+import importlib
+
+from django.core.management import execute_from_command_line
+from django.conf import settings as django_settings
+
+
+def load_django_settings():
+    settings = {}
+    kwargs = {}
+    mods = [importlib.import_module('admin.settings')]
+
+    if hasattr(mods[0], 'load_settings'):
+        getattr(mods[0], 'load_settings')(settings, **kwargs)
+
+    django_settings.configure(**settings)
+
+
+def execute():
+    load_django_settings()
+    execute_from_command_line()
+
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "admin.settings")
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError:
-        # The above import may fail for some other reason. Ensure that the
-        # issue is really that Django is missing to avoid masking other
-        # exceptions on Python 2.
-        try:
-            import django
-        except ImportError:
-            raise ImportError(
-                "Couldn't import Django. Are you sure it's installed and "
-                "available on your PYTHONPATH environment variable? Did you "
-                "forget to activate a virtual environment?"
-            )
-        raise
-    execute_from_command_line(sys.argv)
+    execute()
