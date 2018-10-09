cf6dd951ee5a605833c74b9765740e15fefdf2d4
klm123 <klm123@163.com>
2018-10-09 02:09:03 -0700

    add api test

--- applications/operation/views/operation_views.py
+++ applications/operation/views/operation_views.py
@@ -2,9 +2,18 @@
 from django.contrib.auth.views import LoginView
 from django.http import HttpResponse
 
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+
 
 def lvs(request):
-    return HttpResponse("ok")
+    users = User.objects.all()
+
+    result = dict()
+    for user in users:
+        result[user.username] = user.password
+
+    return HttpResponse(result)
 
 
 def index(request):
